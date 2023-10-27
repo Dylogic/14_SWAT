95064eaebee57270efd0c9a662e0e298be51a767
Dylo <dylogic06@gmail.com>
2023-10-27 10:31:03 +0200

    Basic working

---
+++ resources/views/episode.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="container">
+        <h2>{{ $episode["name"] }}</h2>
+        <h3>Season {{ $episode["season"] }} episode {{$episode["number"]}}</h3>
+        <img style="max-width: 100%" src="{{ $episode["image"]["original"] }}">
+        <div>
+            <p><b>Airdate:</b> {{ $episode["airdate"] }} {{$episode["airtime"]}}</p>
+            <p><b>Rating:</b> {{ $episode["rating"]["average"] }}</p>
+            <p><b>Runtime:</b> {{ $episode["runtime"] }}</p>
+        </div>
+        <div>
+            {!! $episode["summary"] !!}
+        </div>
+    </div>
+</x-layout>
